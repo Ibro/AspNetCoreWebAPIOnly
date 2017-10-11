fb3000a2cc6f36907c352f9a20ea469555b90f81
Ibro <ibro@ibro.me>
2017-10-11 05:47:16 +0200

    Web API only with ASP.NET Core 2

--- AspNetCoreWebAPIOnly/Startup.cs
+++ AspNetCoreWebAPIOnly/Startup.cs
@@ -1,34 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
 
 namespace AspNetCoreWebAPIOnly
 {
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var mvcCoreBuilder = services.AddMvcCore();
+
+            mvcCoreBuilder
+                .AddFormatterMappings()
+                .AddJsonFormatters()
+                .AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+        {  
+            app.UseMvc();
         }
     }
 }
